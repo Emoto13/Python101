940b7d120e225730a89cefcf004b93dbe883def9
Emoto13 <emcho_spassov@abv.bg>
2020-04-30 13:33:38 +0300

    Update ships_queries.sql

--- week9/ships_queries.sql
+++ week9/ships_queries.sql
@@ -1,19 +1,16 @@
 1.SELECT s.name, c.country, c.numguns,  s.launched
 FROM SHIPS as s
-INNER JOIN CLASSES as c
-WHERE c.class = s.class
+INNER JOIN CLASSES as c ON c.class = s.class
 
 2.SELECT s.name,c.class,  c.country, c.numguns,  s.launched
 FROM SHIPS as s
 INNER JOIN CLASSES as c
-WHERE c.class = s.class
-OR s.name = c.class
+ON c.class = s.class OR s.name = c.class
 
 3.SELECT o.ship
 FROM OUTCOMES as o
-INNER JOIN BATTLES as b
-WHERE o.battle = b.name
-AND b.date LIKE '%1942%'
+INNER JOIN BATTLES as b ON o.battle = b.name
+WHERE b.date LIKE '%1942%'
 
 4.SELECT DISTINCT s.name, c.country, c.class
 FROM SHIPS AS s
