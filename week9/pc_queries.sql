18b2b27beb871527870873572713755528c19c15
Emoto13 <emcho_spassov@abv.bg>
2020-05-01 21:39:11 +0300

    Update queries

--- week9/pc_queries.sql
+++ week9/pc_queries.sql
@@ -3,23 +3,23 @@ FROM PC
 
 2.SELECT maker, AVG(l.screen)
 FROM PRODUCT as p
-INNER JOIN LAPTOP as l
-WHERE p.model = l.model
+INNER JOIN LAPTOP as l ON p.model = l.model
 GROUP BY p.maker
 
 3.SELECT AVG(speed)
 FROM LAPTOP
 WHERE price > 1000
 
-4.SELECT hd, AVG(price)
+4.SELECT AVG(price)
 FROM PC
 GROUP BY hd
 
 5.SELECT AVG(price)
 FROM PC
 WHERE speed > 500
+GROUP BY speed
 
-6.SELECT prod.maker, AVG(p.price)
+6.SELECT AVG(p.price)
 FROM PC as p
 INNER JOIN PRODUCT as prod ON p.model = prod.model
 WHERE prod.maker = 'A'
@@ -48,22 +48,34 @@ WHERE prod.maker = 'B')))
 FROM product as prod
 LIMIT 1
 
+7.1.SELECT *
+FROM
+(SELECT AVG(price) FROM pc JOIN product ON pc.model=product.model WHERE maker="B"),
+(SELECT AVG(price) FROM laptop JOIN product ON laptop.model=product.model WHERE maker="B")
+
+7.2. SELECT ( Sum(pc.price) + Sum(laptop.price) ) / Count(*)
+FROM product
+LEFT OUTER JOIN pc
+ON product.model = pc.model
+LEFT OUTER JOIN laptop
+ON product.model = laptop.model
+WHERE product.maker = 'B'
+
 8.SELECT prod.maker
 FROM PRODUCT AS prod
 WHERE prod.type = 'PC'
 GROUP BY prod.maker
 HAVING COUNT(prod.model) >= 3
 
-9.SELECT prod.maker
-FROM PRODUCT AS prod
-INNER JOIN PC as p
-WHERE prod.type = 'PC'
-ORDER BY p.price DESC
-LIMIT 1
+9.SELECT AVG(price) as avg_price, maker
+FROM PC
+INNER JOIN PRODUCT as p
+ON pc.model = product.model
+GROUP BY maker
+ORDER BY avg_price DESC
 
-10.SELECT prod.maker, AVG(hd)
+10.SELECT AVG(hd)
 FROM PRODUCT AS prod
-INNER JOIN PRINTER as prt
-INNER JOIN PC as p
-WHERE p.model = prod.model
-GROUP BY prod.maker
+INNER JOIN PC as p ON p.model = prod.model
+WHERE maker in
+(SELECT maker from product WHERE type = 'Printer')
--- week9/ships_queries.sql
+++ week9/ships_queries.sql
@@ -3,9 +3,12 @@ FROM SHIPS as s
 INNER JOIN CLASSES as c ON c.class = s.class
 
 2.SELECT s.name,c.class,  c.country, c.numguns,  s.launched
-FROM SHIPS as s
-INNER JOIN CLASSES as c
-ON c.class = s.class OR s.name = c.class
+FROM CLASSES as c
+LEFT JOIN SHIPS as s
+ON s.name = c.class
+WHERE c.class
+LIKE (SELECT name FROM ships)
+OR s.name IS NOT NULL
 
 3.SELECT o.ship
 FROM OUTCOMES as o
@@ -19,8 +22,10 @@ ON c.class = s.class
 INNER JOIN OUTCOMES as o
 ON s.name = o.ship
 
-5.SELECT s.name, o.BATTLE, c.country
+5.SELECT s.name
 FROM SHIPS AS s
-INNER JOIN OUTCOMES AS o ON o.ship = s.name
-INNER JOIN CLASSES AS c ON c.class = s.class
-WHERE NOT s.name = o.ship
+WHERE s.name NOT IN
+(SELECT ship
+FROM OUTCOMES as o
+INNER JOIN BATTLES as b
+ON o.battles = b.name)
