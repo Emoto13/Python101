791a69d63f15d635d8f8ec632d9ff3282c07b997
Emoto13 <emcho_spassov@abv.bg>
2020-05-03 12:53:40 +0300

    Add event_handler.py

---
+++ week9/01.Business_Card_Catalog/event_handler.py
@@ -0,0 +1,23 @@
+from commands.add_command import add_business_card
+from commands.list_command import list_users
+from commands.get_command import get_user_by_id
+from commands.delete_command import delete_user_by_id
+from commands.help_command import execute_help_command
+
+
+def handle_command(command):
+    commands = {'add': add_business_card,
+                'list': list_users,
+                'get': get_user_by_id,
+                'delete': delete_user_by_id,
+                'help': execute_help_command}
+
+    return handle_wrapper(command, commands)
+
+
+def handle_wrapper(command, commands):
+    if command not in commands.keys():
+        raise ValueError('No such command')
+
+    execute_command = commands[command]
+    return execute_command()
