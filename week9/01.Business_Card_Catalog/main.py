b23788f322677e2077e7477993757cc4e358cb04
Emoto13 <emcho_spassov@abv.bg>
2020-05-03 12:53:57 +0300

    Add main.py

---
+++ week9/01.Business_Card_Catalog/main.py
@@ -0,0 +1,10 @@
+from factory import enter_command
+from create_database import create_database
+
+def main():
+    create_database()
+    enter_command()
+
+
+if __name__ == '__main__':
+    main()
