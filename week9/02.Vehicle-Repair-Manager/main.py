97d53bbde8d9c07bdcf5e87d3f319da534cd2c33
Emoto13 <emcho_spassov@abv.bg>
2020-05-03 21:41:03 +0300

    Add main.py

---
+++ week9/02.Vehicle-Repair-Manager/main.py
@@ -0,0 +1,12 @@
+from create_db_and_tables import create_database_and_tables
+from client_queries import client_factory
+from list_free_hours_by_date_command import list_free_hours_by_date
+
+
+def main():
+    create_database_and_tables()
+    list_free_hours_by_date('03-05-2020')
+
+
+if __name__ == '__main__':
+    main()
