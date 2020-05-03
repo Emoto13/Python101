d0d9dae7d8f784a0ed4c0a7ab311dd06418cef37
Emoto13 <emcho_spassov@abv.bg>
2020-05-03 12:53:00 +0300

    Add create_database.py

---
+++ week9/01.Business_Card_Catalog/create_database.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+
+def create_database():
+    connection = sqlite3.connect('business_catalog.db')
+    cursor = connection.cursor()
+    query = '''CREATE TABLE IF NOT EXISTS users
+    (id INTEGER primary key AUTOINCREMENT unique not NULL,
+    full_name varchar(50) unique not NULL,
+    email varchar(50) unique not NULL,
+    age integer Not NULL, 
+    phone varchar(50) not NULL,
+    additional_info TEXT)'''
+    cursor.execute(query)
+    connection.commit()
+    connection.close()
