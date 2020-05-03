d0fd049a77cb870976c51c26f279ee0bb137c31f
Emoto13 <emcho_spassov@abv.bg>
2020-05-03 21:45:54 +0300

    Update imports

--- week9/02.Vehicle-Repair-Manager/main.py
+++ week9/02.Vehicle-Repair-Manager/main.py
@@ -1,5 +1,5 @@
-from create_db_and_tables import create_database_and_tables
-from client_queries import client_factory
+from create_database_and_tables import create_database_and_tables
+from client_factory import client_factory
 from list_free_hours_by_date_command import list_free_hours_by_date
 
 
